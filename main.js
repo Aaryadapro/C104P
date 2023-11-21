f4b0d40b82f2d195c0c7ab5bff33e71d185f451a
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-11-21 18:54:08 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-Webcam.set({
+function setup(){
+    Webcam.set({
     width:250,
     height:250,
     image_format:"png",
     png_quality:90
 });
+}
             
 camera = document.getElementById("camera");
  
